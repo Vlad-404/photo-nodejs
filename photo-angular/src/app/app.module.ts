39ee1b3cce2b957ac6129a847d49607e1440e179
Vlad-404 <vmijat21@gmail.com>
2022-07-28 14:08:21 +0200

    Add nav-mobile-component

--- photo-angular/src/app/app.module.ts
+++ photo-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavDesktopComponent } from './navbar/desktop/nav-desktop.component'
+import { NavMobileComponent } from './navbar/mobile/nav-mobile.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavDesktopComponent
+    NavDesktopComponent,
+    NavMobileComponent
   ],
   imports: [
     BrowserModule,
---
+++ photo-angular/src/app/navbar/mobile/nav-mobile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-nav-mobile',
+    templateUrl: './nav-mobile.component.html'
+})
+
+export class NavMobileComponent {
+
+}

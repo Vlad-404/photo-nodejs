296915e241061317088f25d3a2fc92ce8eecbddd
Vlad-404 <vmijat21@gmail.com>
2022-07-28 13:51:37 +0200

    Add nav-desktop component

--- photo-angular/src/app/app.component.ts
+++ photo-angular/src/app/app.component.ts
@@ -8,3 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'photo-angular';
 }
+
+console.log('Script file loaded');
--- photo-angular/src/app/app.module.ts
+++ photo-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavDesktopComponent } from './navbar/desktop/nav-desktop.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavDesktopComponent
   ],
   imports: [
     BrowserModule,

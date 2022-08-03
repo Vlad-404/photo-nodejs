80c053c98c9ef1f6b702a4db1001aaa145a895af
Vlad-404 <vmijat21@gmail.com>
2022-08-03 18:34:03 +0200

    Add titles and images for galleries

--- photo-angular/src/app/gallery-home/gallery-home.component.ts
+++ photo-angular/src/app/gallery-home/gallery-home.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class GalleryHomeComponent {
-  
+  galleries = [
+    {title: "Architecture", imageUrl: "https://res.cloudinary.com/drtxn8d5t/image/upload/v1598659115/ip5puuoub18swxhrd7nw.jpg"},
+    {title: "Seasons", imageUrl: "https://res.cloudinary.com/drtxn8d5t/image/upload/v1598656884/gyw7ttigjtdl0qhmnzru.jpg"},
+    {title: "Food", imageUrl: "https://res.cloudinary.com/drtxn8d5t/image/upload/v1607002040/IMG_20181124_204424_la1pnz.jpg"},
+    {title: "Landscapes", imageUrl: "https://res.cloudinary.com/drtxn8d5t/image/upload/v1607345621/photo-portfolio/DSC_0140_iisobc.jpg"}
+  ]
 }

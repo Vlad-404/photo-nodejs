0ac2fe4a729135f99330d7f8dc130e82b0127ff2
Vlad-404 <vmijat21@gmail.com>
2022-08-03 17:09:16 +0200

    Add and format the carousel image

--- photo-angular/src/app/hero-carousel/hero-carousel.component.ts
+++ photo-angular/src/app/hero-carousel/hero-carousel.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-hero-carousel',
-    templateUrl: './hero-carousel.component.html'
+    templateUrl: './hero-carousel.component.html',
+    styleUrls: ['./hero-carousel.component.css']
 })
 
 export class HeroCarouselComponent {

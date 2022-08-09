7882c8e86031778499340f8f2a3a441ea5546ef8
Vlad-404 <vmijat21@gmail.com>
2022-08-09 15:30:15 +0200

    Add Mongoose for MongoDB

---
+++ photo-angular/src/app/galleries/galleries.model.ts
@@ -0,0 +1,14 @@
+export interface Galleries {
+    id: string,
+    title: string,
+    description: string,
+    category: string,
+    panorama: boolean,
+    color: boolean,
+    price: number,
+    rating: number,
+    imageUrl: string,
+    imgId: string,
+    notes: string,
+    uploadedBy: string
+}

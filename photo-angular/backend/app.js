1da44ccb499b87e02e5cc1bdc91f8d28991e87bb
Vlad-404 <vmijat21@gmail.com>
2022-08-12 15:26:34 +0200

    Move local galleries from app.js to separate file

--- photo-angular/backend/app.js
+++ photo-angular/backend/app.js
@@ -71,66 +71,6 @@ app.get('/api/galleries', (req, res, next) => {
                 message: "Error fetching images!"
             })
         });
-
-    const localGalleries = [
-        {
-            id: 'fyaid86%70a',
-            title: 'Dusseldorf Waterfront',
-            description: 'Modern architecture at Dusseldorfer waterfront',
-            category: 'Architecture',
-            panorama: 'false',
-            color: 'true',
-            price: '15,99',
-            rating: '3,70',
-            imageUrl: 'https://res.cloudinary.com/drtxn8d5t/image/upload/v1598659115/ip5puuoub18swxhrd7nw.jpg',
-            imageId: 'ip5puuoub18swxhrd7nw',
-            notes: 'Notes not visible to public',
-            uploadedBy: 'author'
-        },
-        {
-            id: 'fy#jr7O6%70a',
-            title: 'Geneva Lake',
-            description: 'Geneva lake at Winter',
-            category: 'Seasons',
-            panorama: 'false',
-            color: 'true',
-            price: '15,99',
-            rating: '3,70',
-            imageUrl: 'https://res.cloudinary.com/drtxn8d5t/image/upload/v1598656884/gyw7ttigjtdl0qhmnzru.jpg',
-            imageId: 'gyw7ttigjtdl0qhmnzru',
-            notes: 'Notes not visible to public',
-            uploadedBy: 'author'
-        },
-        {
-            id: 'fy#jr7O89&=lku',
-            title: 'Mozart Cake Slice',
-            description: 'A delicious slice of Mozart cake',
-            category: 'Food',
-            panorama: 'false',
-            color: 'true',
-            price: '15,99',
-            rating: '3,70',
-            imageUrl: 'https://res.cloudinary.com/drtxn8d5t/image/upload/v1607002040/IMG_20181124_204424_la1pnz.jpg',
-            imageId: 'la1pnz',
-            notes: 'Notes not visible to public',
-            uploadedBy: 'author'
-        },
-        {
-            id: 'a6YtgO89&=lku',
-            title: 'Sunset in Swiss Alps',
-            description: 'A sunset in Swiss alps',
-            category: 'Landscapes',
-            panorama: 'false',
-            color: 'true',
-            price: '15,99',
-            rating: '3,70',
-            imageUrl: 'https://res.cloudinary.com/drtxn8d5t/image/upload/v1607345621/photo-portfolio/DSC_0140_iisobc.jpg',
-            imageId: 'iisobc',
-            notes: 'Notes not visible to public',
-            uploadedBy: 'author'
-        }
-        
-    ]
 });
 
 module.exports = app;
